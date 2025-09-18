24091070463ed520077ad77238e7f6197637a9fa
Sutou Kouhei <kou@clear-code.com>
2025-09-18 11:56:51 +0900

    Use gersemi as CMake formatter
    Velox uses gersemi:
    * https://github.com/BlankSpruce/gersemi
    * https://github.com/facebookincubator/velox/pull/14237
    
    cmake-format isn't maintained for a few years.
    See also: https://github.com/facebookincubator/velox/issues/14209
    
    This also uses pre-commit like Velox does:
    https://github.com/facebookincubator/velox/pull/13361
    
    If we use pre-commit, we can pin formatter version by `rev`
    parameter. pre-commit installs the specified version of gersemi
    automatically.
    
    The current `scripts/format-check.py` uses `cmake-format` installed in
    system. Developers must install expected version of `cmake-format`
    manually.

--- CMake/FindFlatBuffers.cmake
+++ CMake/FindFlatBuffers.cmake
@@ -30,24 +30,29 @@ find_program(FLATBUFFERS_FLATC_EXECUTABLE NAMES flatc)
 find_path(FLATBUFFERS_INCLUDE_DIR NAMES flatbuffers/flatbuffers.h)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(FlatBuffers
-  DEFAULT_MSG FLATBUFFERS_FLATC_EXECUTABLE FLATBUFFERS_INCLUDE_DIR)
+find_package_handle_standard_args(
+  FlatBuffers
+  DEFAULT_MSG
+  FLATBUFFERS_FLATC_EXECUTABLE
+  FLATBUFFERS_INCLUDE_DIR
+)
 
 if(FLATBUFFERS_FOUND)
   function(FLATBUFFERS_GENERATE_C_HEADERS Name)
     set(FLATC_OUTPUTS)
     foreach(FILE ${ARGN})
       get_filename_component(FLATC_OUTPUT ${FILE} NAME_WE)
-      set(FLATC_OUTPUT
-        "${CMAKE_CURRENT_BINARY_DIR}/${FLATC_OUTPUT}_generated.h")
+      set(FLATC_OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/${FLATC_OUTPUT}_generated.h")
       list(APPEND FLATC_OUTPUTS ${FLATC_OUTPUT})
 
-      add_custom_command(OUTPUT ${FLATC_OUTPUT}
+      add_custom_command(
+        OUTPUT ${FLATC_OUTPUT}
         COMMAND ${FLATBUFFERS_FLATC_EXECUTABLE}
         ARGS -c -o "${CMAKE_CURRENT_BINARY_DIR}/" ${FILE}
         DEPENDS ${FILE}
         COMMENT "Building C++ header for ${FILE}"
-        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+      )
     endforeach()
     set(${Name}_OUTPUTS ${FLATC_OUTPUTS} PARENT_SCOPE)
   endfunction()
--- CMake/absl.cmake
+++ CMake/absl.cmake
@@ -4,20 +4,20 @@ include_guard(GLOBAL)
 # Velox's resolve dependency module for now. We should change and have our own
 # in the future.
 set(VELOX_ABSL_VERSION 20240116.0)
-set(VELOX_ABSL_BUILD_SHA256_CHECKSUM
-    "338420448b140f0dfd1a1ea3c3ce71b3bc172071f24f4d9a57d59b45037da440")
-set(VELOX_ABSL_SOURCE_URL
-    "https://github.com/abseil/abseil-cpp/archive/refs/tags/${VELOX_ABSL_VERSION}.tar.gz"
+set(
+  VELOX_ABSL_BUILD_SHA256_CHECKSUM
+  "338420448b140f0dfd1a1ea3c3ce71b3bc172071f24f4d9a57d59b45037da440"
+)
+set(
+  VELOX_ABSL_SOURCE_URL
+  "https://github.com/abseil/abseil-cpp/archive/refs/tags/${VELOX_ABSL_VERSION}.tar.gz"
 )
 
 velox_resolve_dependency_url(ABSL)
 
 message(STATUS "Building Abseil from source")
 
-FetchContent_Declare(
-  absl
-  URL ${VELOX_ABSL_SOURCE_URL}
-  URL_HASH ${VELOX_ABSL_BUILD_SHA256_CHECKSUM})
+FetchContent_Declare(absl URL ${VELOX_ABSL_SOURCE_URL} URL_HASH ${VELOX_ABSL_BUILD_SHA256_CHECKSUM})
 
 set(ABSL_BUILD_TESTING OFF)
 set(ABSL_PROPAGATE_CXX_STD ON)
