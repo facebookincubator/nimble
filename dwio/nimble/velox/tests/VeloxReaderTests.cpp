961a113c2c45c7b537d2573f25c105e66ec670a7
David-C-L <david.dave4248@gmail.com>
2026-02-09 20:48:53 +0000

    fix compilation bug in VeloxReaderTests comparing non-numeric to int 0

--- dwio/nimble/velox/tests/VeloxReaderTests.cpp
+++ dwio/nimble/velox/tests/VeloxReaderTests.cpp
@@ -528,8 +528,10 @@ class VeloxReaderTests : public ::testing::TestWithParam<bool> {
 
   template <typename T>
   void getFieldDefaultValue(nimble::Vector<T>& input, uint32_t index) {
-    static_assert(
-        T() == 0, "Default Constructor value is not zero initialized");
+    if constexpr (std::is_arithmetic_v<T>) {
+      static_assert(
+          T() == 0, "Default Constructor value is not zero initialized");
+    }
     input[index] = T();
   }
 
