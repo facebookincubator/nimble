5d9dfe758427a0a428d9263d6e5231fe71a699a2
Suryadev Sahadevan Rajesh <suryadev@meta.com>
2026-06-11 11:02:12 -0700

    Update EncodingFuzzerTest.cpp

--- dwio/nimble/fuzzer/encoding/EncodingFuzzerTest.cpp
+++ dwio/nimble/fuzzer/encoding/EncodingFuzzerTest.cpp
@@ -356,7 +356,7 @@ TYPED_TEST(SubIntSplitFuzzerTest, correctness) {
       FLAGS_fuzzer_compression);
   fuzzer.run();
 }
-#endif  // NIMBLE_ENABLE_EXPERIMENTAL_ENCODINGS
+#endif // NIMBLE_ENABLE_EXPERIMENTAL_ENCODINGS
 
 // FOR: integral types only (float/string are excluded by static_assert)
 using ForTypes = ::testing::Types<
@@ -399,7 +399,7 @@ class FPEPerTierBitmapsFuzzerTest : public ::testing::Test {};
 TYPED_TEST_SUITE(FPEPerTierBitmapsFuzzerTest, FPETypes);
 
 TYPED_TEST(FPEPerTierBitmapsFuzzerTest, correctness) {
-  Encoding::Options opts{};
+  Encoding::Options opts;
   opts.frequencyPartitionIndex =
       static_cast<uint8_t>(FreqPartIndexType::PerTierBitmaps);
   EncodingFuzzer<TypeParam> fuzzer(
@@ -416,7 +416,7 @@ class FPETierTagArrayFuzzerTest : public ::testing::Test {};
 TYPED_TEST_SUITE(FPETierTagArrayFuzzerTest, FPETypes);
 
 TYPED_TEST(FPETierTagArrayFuzzerTest, correctness) {
-  Encoding::Options opts{};
+  Encoding::Options opts;
   opts.frequencyPartitionIndex =
       static_cast<uint8_t>(FreqPartIndexType::TierTagArray);
   EncodingFuzzer<TypeParam> fuzzer(
@@ -433,7 +433,7 @@ class FPEEliasFanoFuzzerTest : public ::testing::Test {};
 TYPED_TEST_SUITE(FPEEliasFanoFuzzerTest, FPETypes);
 
 TYPED_TEST(FPEEliasFanoFuzzerTest, correctness) {
-  Encoding::Options opts{};
+  Encoding::Options opts;
   opts.frequencyPartitionIndex =
       static_cast<uint8_t>(FreqPartIndexType::EliasFano);
   EncodingFuzzer<TypeParam> fuzzer(
